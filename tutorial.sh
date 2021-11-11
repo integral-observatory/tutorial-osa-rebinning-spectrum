96cd38f522cbc932080ff7e166fbdd82c657c50e
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-11-11 12:42:29 +0100

    split in functions

--- tutorial.sh
+++ tutorial.sh
@@ -9,58 +9,101 @@
 
 # please refer to heasoft manual on how to construct this bin definition
 # https://heasarc.gsfc.nasa.gov/docs/software/ftools/caldb/rbnrmf.html
-cat > bins.txt <<HERE
- 0   9   -1
- 10  409  8
- 410 2047 -1
+
+set -e
+
+[ -z ${ISDC_ENV+x} ] && { echo "no ISDC_ENV!"; exit 1; }
+ibis_isgr_energy --version | grep 9.1.2 > /dev/null || { echo "old ibis_isgr_energy!"; exit 1; }
+
+    
+
+# test case
+export scw=221500540010.001
+
+
+function construct_broad_bin_rmf {
+    # The question of role of optimal grouping is somewhat debated. 
+    # Grouping kills information, so it's normally not useful. But it can also strategically hide some information, with desirable effects.
+    # See some details in https://arxiv.org/abs/1601.05309
+
+    # Grouping Except just for visualization. You can also do this exact kind of grouping in xspec.
+    # 9 counts is not enough to use gaussian approximation. Also this sort of adapting grouping creates bins as random process, which makes it difficult to combine them later
+
+    cat > bins.txt <<HERE
+     0   9   -1
+     10  409  8
+     410 2047 -1
 HERE
 
 # check your heasoft versions. Note that between versions 6.24 and 6.28, there was a rapid evolution of this particular functionality in Heasoft. 
 # This tutorial applies to 6.24. You 
-fversion
+    fversion
 
 # original matrix has 2048 channel bins
-fstruct $REP_BASE_PROD/ic/ibis/rsp/isgr_rmf_grp_0025.fits[ISGR-EBDS-MOD]
+    fstruct $REP_BASE_PROD/ic/ibis/rsp/isgr_rmf_grp_0025.fits[ISGR-EBDS-MOD]
 
-new_rmf_fn=$PWD/new-rmf.fits
+    new_rmf_fn=$PWD/new-rmf.fits
 
-rbnrmf \
-    infile="$REP_BASE_PROD/ic/ibis/rsp/isgr_rmf_grp_0025.fits" \
-    outfile=$new_rmf_fn \
-    binfile="bins.txt" \
-    clobber=yes
+    rbnrmf \
+        infile="$REP_BASE_PROD/ic/ibis/rsp/isgr_rmf_grp_0025.fits" \
+        outfile=$new_rmf_fn \
+        binfile="bins.txt" \
+        clobber=yes
 
 # original matrix has 2048 channel bins
-fstruct $new_rmf_fn[ISGR-EBDS-MOD]
+    fstruct $new_rmf_fn[ISGR-EBDS-MOD]
 
+}
+
+function in_broad_bins {
 
 # analyse the spectrum with these bins
+    construct_broad_bin_rmf
 
-export scw=198700220010.001
 
-ls $REP_BASE_PROD/scw/${scw::4}/$scw/swg.fits > scws.txt
+    ls $REP_BASE_PROD/scw/${scw::4}/$scw/swg.fits > scws.txt
 
-rm -rfv obs/test-bins
+    rm -rfv obs/test-bins
 
-export COMMONLOGFILE=+$PWD/commonlog.txt
-export COMMONSCRIPT=1
+    export COMMONLOGFILE=+$PWD/commonlog.txt
+    export COMMONSCRIPT=1
 
 # Standard OSA
 
-og_create \
-    idxSwg=scws.txt \
-    instrument=ibis \
-    ogid=test-bins \
-    baseDir=$PWD
+    og_create \
+        idxSwg=scws.txt \
+        instrument=ibis \
+        ogid=test-bins \
+        baseDir=$PWD
 
-(
-    cd obs/test-bins
-    ibis_science_analysis \
-        startLevel=COR \
-        endLevel=SPE \
-        IBIS_SI_inEnergyValues=$new_rmf_fn[ISGR-EBDS-MOD]
-)
+    (
+        cd obs/test-bins
+        ibis_science_analysis \
+            startLevel=COR \
+            endLevel=SPE \
+            IBIS_SI_inEnergyValues="$new_rmf_fn[ISGR-EBDS-MOD]"
+    )
 
 # Inspect the spectrum  and make sure it has the desired number of bins
 
-fstruct obs/test-bins/scw/$scw/isgri_spectrum.fits[2]
+    fstruct obs/test-bins/scw/$scw/isgri_spectrum.fits[2]
+}
+
+
+function inspect_spectra {
+    ( cut -c5- | python ) <<HERE
+    from astropy.io import fits
+    f = fits.open("obs/test-bins/scw/$scw/isgri_spectrum.fits")
+    d = f[3]
+    print(d.data['RATE'])
+    
+    print(d.data['RATE']*d.header['EXPOSURE'])
+HERE
+}
+
+if [ -z $@ ]; then
+    echo 'please pick one of the following commands:'
+    cat "${BASH_SOURCE[0]}" | awk '/^function/ {print " * bash '"${BASH_SOURCE[0]}"' "$2}'
+else
+    $@
+fi
